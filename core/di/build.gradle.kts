2aed413f207a3e1ce89d8214c62da879b2fe3a3a
sejun2 <qpfjf56@gmail.com>
2024-11-19 14:45:27 +0900

    add dependency  - core:navigation module

--- core/di/build.gradle.kts
+++ core/di/build.gradle.kts
@@ -15,6 +15,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(projects.core.navigation)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
