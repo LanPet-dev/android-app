8e862d922f32f49f484c13f9d4dbcecf6032acdf
sejun2 <qpfjf56@gmail.com>
2024-11-25 20:20:29 +0900

    add freeboard feature

--- feature/free/src/main/java/com/lanpet/free/FreeBoardScreen.kt
+++ feature/free/src/main/java/com/lanpet/free/FreeBoardScreen.kt
@@ -1,2 +1,19 @@
 package com.lanpet.free
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import com.example.designsystem.theme.LanPetAppTheme
+
+@Composable
+fun FreeBoardScreen() {
+    Text("FreeBoardScreen")
+}
+
+@PreviewLightDark
+@Composable
+fun FreeBoardScreenPreview() {
+    LanPetAppTheme {
+        FreeBoardScreen()
+    }
+}
--- feature/free/src/main/java/com/lanpet/free/navigation/FreeNavGraph.kt
+++ feature/free/src/main/java/com/lanpet/free/navigation/FreeNavGraph.kt
@@ -1,4 +1,34 @@
 package com.lanpet.free.navigation
 
-class FreeNavGraph {
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import androidx.navigation.navigation
+import com.lanpet.free.FreeBoardScreen
+import kotlinx.serialization.Serializable
+
+fun NavGraphBuilder.freeNavGraph() {
+    navigation<FreeBoardBaseRoute>(
+        startDestination = FreeBoard,
+    ) {
+        composable<FreeBoard>() {
+            FreeBoardScreen()
+        }
+    }
 }
+
+fun NavController.navigateToFreeBoardScreen() {
+    navigate(
+        FreeBoard,
+    ) {
+        popUpTo(0) {
+            inclusive = true
+        }
+    }
+}
+
+@Serializable
+object FreeBoardBaseRoute
+
+@Serializable
+object FreeBoard
