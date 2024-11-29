394bdbfa5e158e225a28cb17a55c1e6af6bdd135
sejun2 <qpfjf56@gmail.com>
2024-11-29 14:15:37 +0900

    refactor commonbutton factory

--- core/designsystem/src/main/java/com/lanpet/core/designsystem/theme/widgets/CommonButton.kt
+++ core/designsystem/src/main/java/com/lanpet/core/designsystem/theme/widgets/CommonButton.kt
@@ -1,6 +1,9 @@
 package com.lanpet.core.designsystem.theme.widgets
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -16,8 +19,69 @@ import com.lanpet.core.designsystem.theme.LanPetAppTheme
 import com.lanpet.core.designsystem.theme.LanPetDimensions
 import com.lanpet.core.designsystem.theme.customColorScheme
 
+enum class ButtonSize {
+    SMALL,
+    MEDIUM,
+    LARGE
+}
+
 @Composable
-fun CommonButton(modifier: Modifier = Modifier, title: String, onClick: (() -> Unit)?) {
+fun CommonButton(
+    modifier: Modifier = Modifier,
+    title: String,
+    buttonSize: ButtonSize = ButtonSize.MEDIUM,
+    onClick: (() -> Unit)?
+) {
+    when (buttonSize) {
+        ButtonSize.SMALL -> {
+            CommonButtonSmall(
+                title = title,
+                modifier = modifier,
+                onClick = onClick
+            )
+        }
+
+        ButtonSize.MEDIUM -> {
+            CommonButtonMedium(
+                title = title,
+                modifier = modifier,
+                onClick = onClick
+            )
+        }
+
+        ButtonSize.LARGE -> {
+            CommonButtonLarge(
+                title = title,
+                modifier = modifier,
+                onClick = onClick
+            )
+        }
+    }
+}
+
+@Composable
+internal fun CommonButtonSmall(modifier: Modifier = Modifier, title: String, onClick: (() -> Unit)?) {
+    Button(
+        shape = RoundedCornerShape(LanPetDimensions.Corner.xSmall),
+        onClick = {
+            onClick?.invoke()
+        },
+        colors = ButtonDefaults.buttonColors().copy(
+            containerColor = MaterialTheme.customColorScheme.buttonBackground
+        ),
+        modifier = modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+    ) {
+        Text(
+            title,
+            style = MaterialTheme.typography.bodyMedium
+        )
+    }
+}
+
+@Composable
+internal fun CommonButtonMedium(modifier: Modifier = Modifier, title: String, onClick: (() -> Unit)?) {
     Button(
         shape = RoundedCornerShape(LanPetDimensions.Corner.xSmall),
         onClick = {
@@ -38,14 +102,52 @@ fun CommonButton(modifier: Modifier = Modifier, title: String, onClick: (() -> U
     }
 }
 
+@Composable
+internal fun CommonButtonLarge(modifier: Modifier = Modifier, title: String, onClick: (() -> Unit)?) {
+    Button(
+        shape = RoundedCornerShape(LanPetDimensions.Corner.xSmall),
+        onClick = {
+            onClick?.invoke()
+        },
+        colors = ButtonDefaults.buttonColors().copy(
+            containerColor = MaterialTheme.customColorScheme.buttonBackground
+        ),
+        modifier = modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .sizeIn(minHeight = 64.dp)
+    ) {
+        Text(
+            title,
+            style = MaterialTheme.typography.bodyMedium
+        )
+    }
+}
+
+
 @PreviewLightDark
 @Composable()
 fun PreviewCommonButton() {
     LanPetAppTheme {
-        CommonButton(
-            title = "This is title",
-            modifier = Modifier,
-            onClick = {}
-        )
+        Column {
+
+            CommonButtonSmall(
+                title = "This is title",
+                modifier = Modifier,
+                onClick = {}
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            CommonButtonMedium(
+                title = "This is title",
+                modifier = Modifier,
+                onClick = {}
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            CommonButtonLarge(
+                title = "This is title",
+                modifier = Modifier,
+                onClick = {}
+            )
+        }
     }
 }
