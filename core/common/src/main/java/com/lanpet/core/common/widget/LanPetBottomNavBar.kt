91b927314666e0ef0f24869abbf06bb9b85fdc14
sejun2 <qpfjf56@gmail.com>
2024-12-02 02:41:39 +0900

    fix initial navitem

--- core/common/src/main/java/com/lanpet/core/common/widget/LanPetBottomNavBar.kt
+++ core/common/src/main/java/com/lanpet/core/common/widget/LanPetBottomNavBar.kt
@@ -59,7 +59,7 @@ fun LanPetBottomNavBar(
 
 @Composable
 fun rememberBottomNavItem() = remember {
-    mutableStateOf(BottomNavItem.Initial)
+    mutableStateOf(BottomNavItem.Wiki)
 }
 
 @PreviewLightDark
