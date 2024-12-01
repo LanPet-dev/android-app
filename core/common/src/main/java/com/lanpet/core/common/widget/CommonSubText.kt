8693c35f53d54fd620f3ef48c6d3eb130a7a9e3e
sejun2 <qpfjf56@gmail.com>
2024-12-02 03:01:18 +0900

    add subtext widget

--- core/common/src/main/java/com/lanpet/core/common/widget/CommonSubText.kt
+++ core/common/src/main/java/com/lanpet/core/common/widget/CommonSubText.kt
@@ -1,4 +1,23 @@
 package com.lanpet.core.common.widget
 
-class CommonSubText {
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.lanpet.core.designsystem.theme.customColorScheme
+import com.lanpet.core.designsystem.theme.customTypography
+
+@Composable
+fun DuplicatedNicknameOkText(title: String) {
+    Text(
+        title,
+        style = MaterialTheme.customTypography().body3MediumSingle.copy(color = MaterialTheme.customColorScheme.confirmText),
+    )
+}
+
+@Composable
+fun DuplicatedNicknameErrorText(title: String) {
+    Text(
+        title,
+        style = MaterialTheme.customTypography().body3MediumSingle.copy(color = MaterialTheme.customColorScheme.errorText),
+    )
 }
