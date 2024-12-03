5d43224df51dfe4805c40a158ee54011b0bde109
sejun2 <qpfjf56@gmail.com>
2024-12-04 00:20:55 +0900

    add Post model & test

--- domain/model/build.gradle.kts
+++ domain/model/build.gradle.kts
@@ -2,19 +2,17 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.convention.lib.build)
+    alias(libs.plugins.convention.lib.junit5)
 }
 
 android {
     namespace = "com.example.model"
+
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    testImplementation(libs.junit)
-    testImplementation(libs.junit.jupiter)
-    androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
--- domain/model/src/main/java/com/example/model/Post.kt
+++ domain/model/src/main/java/com/example/model/Post.kt
@@ -1,16 +1,20 @@
 package com.example.model
 
 import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
 import java.util.Locale
 import java.util.TimeZone
 
 data class Post(
+    val id: Int,
     val petCategory: PetCategory,
     val title: String,
     val tags: List<String>,
     val content: String,
     val images: List<String>,
     val createdAt: String,
+    val updatedAt: String,
     val likeCount: Int,
     val commentCount: Int,
 ) {
@@ -30,4 +34,31 @@ data class Post(
                 return ""
             }
         }
+}
+
+fun Post.createdAtPostString(
+    currentTime: Date = Date()
+): String {
+    try {
+        val inputFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX", Locale.getDefault())
+        inputFormat.timeZone = TimeZone.getTimeZone("UTC")
+
+
+        val date = inputFormat.parse(createdAt) ?: return ""
+
+        assert(currentTime >= date)
+
+        val timeDiff = (currentTime.time - date.time) / 1000 // 초 단위 차이
+
+        return when {
+            timeDiff < 60 -> "방금 전"
+            timeDiff < 60 * 60 -> "${timeDiff / 60}분 전"
+            timeDiff < 60 * 60 * 24 -> "${timeDiff / (60 * 60)}시간 전"
+            timeDiff < 60 * 60 * 24 * 30 -> "${timeDiff / (60 * 60 * 24)}일 전"
+            timeDiff < 60 * 60 * 24 * 365 -> "${timeDiff / (60 * 60 * 24 * 30)}달 전"
+            else -> "${timeDiff / (60 * 60 * 24 * 365)}년 전"
+        }
+    } catch (e: Exception) {
+        return ""
+    }
 }
