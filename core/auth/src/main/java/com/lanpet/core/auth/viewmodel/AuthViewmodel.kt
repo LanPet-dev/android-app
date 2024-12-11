0e8b34dca73f4748f75b2e89964e76e3ecedc036
sejun2 <qpfjf56@gmail.com>
2024-12-12 01:35:34 +0900

    refactor handleAuthCode logic

--- core/auth/src/main/java/com/lanpet/core/auth/viewmodel/AuthViewmodel.kt
+++ core/auth/src/main/java/com/lanpet/core/auth/viewmodel/AuthViewmodel.kt
@@ -3,8 +3,6 @@ package com.lanpet.core.auth.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.model.AuthState
-import com.example.model.SocialAuthToken
-import com.example.model.account.Account
 import com.example.usecase.GetAccountInformationUseCase
 import com.example.usecase.GetCognitoSocialAuthTokenUseCase
 import com.example.usecase.RegisterAccountUseCase
@@ -12,10 +10,9 @@ import com.lanpet.core.manager.AuthStateHolder
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.flatMapLatest
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.single
 import kotlinx.coroutines.flow.timeout
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -35,38 +32,36 @@ class AuthViewModel @Inject constructor(
     @OptIn(FlowPreview::class, ExperimentalCoroutinesApi::class)
     fun handleAuthCode(code: String) {
         viewModelScope.launch {
-            getCognitoSocialAuthTokenUseCase(code).timeout(5.seconds)
-                .onEach {
-                    authStateHolder.updateState(AuthState.Loading(it))
-                }
-                .onEach { token ->
-                    authStateHolder.updateState(AuthState.Loading(token))
-                }.flatMapLatest { socialAuthToken ->
-                    getAccountInformationUseCase().timeout(5.seconds)
-                        .map { account ->
-                            Pair(socialAuthToken, account)
-                        }.catch {
-                            registerAccountUseCase().timeout(5.seconds).map {
-                                socialAuthToken
-                            }.flatMapLatest {
-                                getAccountInformationUseCase().timeout(5.seconds).map { account ->
-                                    Pair<SocialAuthToken, Account?>(socialAuthToken, account)
-                                }
-                            }.catch {
-                                emit(Pair(socialAuthToken, null))
-                            }
-                        }
-                }.catch {
-                    authStateHolder.updateState(AuthState.Fail)
-                }.collect {
-                    println("collect called :: $it")
-                    if (it.second == null) {
-                        authStateHolder.updateState(AuthState.Fail)
-                        return@collect
-                    }
+            try {
+                val token = getCognitoSocialAuthTokenUseCase(code).timeout(5.seconds).onEach {
+                    authStateHolder.updateState(
+                        AuthState.Loading(socialAuthToken = it)
+                    )
+                }.first()
 
-                    authStateHolder.updateState(AuthState.Success(it.first, it.second))
+                try {
+                    val account = getAccountInformationUseCase().single()
+                    authStateHolder.updateState(
+                        AuthState.Success(
+                            socialAuthToken = token,
+                            account = account
+                        )
+                    )
+                } catch (e: Exception) {
+                    val accountToken = registerAccountUseCase().single()
+                    val account = getAccountInformationUseCase().single()
+                    authStateHolder.updateState(
+                        AuthState.Success(
+                            socialAuthToken = token,
+                            account = account
+                        )
+                    )
                 }
+            } catch (e: Exception) {
+                authStateHolder.updateState(
+                    AuthState.Fail
+                )
+            }
         }
     }
 
