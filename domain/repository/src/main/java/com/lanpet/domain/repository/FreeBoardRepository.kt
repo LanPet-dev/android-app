00b35381cbda122e77bd74851d8b457d2cfe6919
sejun2 <qpfjf56@gmail.com>
2024-12-12 19:34:28 +0900

    add FreeBoardRepository

--- domain/repository/src/main/java/com/lanpet/domain/repository/FreeBoardRepository.kt
+++ domain/repository/src/main/java/com/lanpet/domain/repository/FreeBoardRepository.kt
@@ -1,4 +1,12 @@
 package com.lanpet.domain.repository
 
+import com.lanpet.domain.model.FreeBoardComment
+import com.lanpet.domain.model.FreeBoardPostDetail
+import com.lanpet.domain.model.FreeBoardPost
+import kotlinx.coroutines.flow.Flow
+
 interface FreeBoardRepository {
+    fun getFreeBoardPostList(): Flow<List<FreeBoardPost>>
+    fun getFreeBoardDetail(id: String): Flow<FreeBoardPostDetail>
+    fun getFreeBoardCommentList(id: String): Flow<List<FreeBoardComment>>
 }
