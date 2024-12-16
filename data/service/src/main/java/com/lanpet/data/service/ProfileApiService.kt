41eff102d2f97a2932dca506ce39c571e0650bb9
sejun2 <qpfjf56@gmail.com>
2024-12-17 01:03:47 +0900

    fix ProfileApiService return value

--- data/service/src/main/java/com/lanpet/data/service/ProfileApiService.kt
+++ data/service/src/main/java/com/lanpet/data/service/ProfileApiService.kt
@@ -1,19 +1,21 @@
 package com.lanpet.data.service
 
+import com.lanpet.data.dto.FindProfileResponse
 import com.lanpet.data.dto.RegisterManProfileRequest
 import com.lanpet.data.dto.RegisterPetProfileRequest
+import com.lanpet.data.dto.RegisterProfileResponse
 import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface ProfileApiService {
     @POST("profiles")
-    suspend fun createManProfile(registerManProfileRequest: RegisterManProfileRequest)
+    suspend fun createManProfile(registerManProfileRequest: RegisterManProfileRequest): RegisterProfileResponse
 
     @POST("profiles")
-    suspend fun createPetProfile(registerPetProfileRequest: RegisterPetProfileRequest)
+    suspend fun createPetProfile(registerPetProfileRequest: RegisterPetProfileRequest): RegisterProfileResponse
 
     @GET("profiles")
-    suspend fun getProfileList()
+    suspend fun getProfileList(): FindProfileResponse
 
     @GET("profiles/{id}")
     suspend fun getProfileDetail(id: String)
