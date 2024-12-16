5283452b3dc2189c6a16018e51d4ccfe117f47f4
sejun2 <qpfjf56@gmail.com>
2024-12-16 20:33:47 +0900

    add todo

--- domain/model/src/main/java/com/lanpet/domain/model/AuthState.kt
+++ domain/model/src/main/java/com/lanpet/domain/model/AuthState.kt
@@ -2,6 +2,7 @@ package com.lanpet.domain.model
 
 import com.lanpet.domain.model.account.Account
 
+// TODO("Satoshi"): Add authState that whether user has own profile or not
 sealed class AuthState {
     data class Success(
         val socialAuthToken: SocialAuthToken?,
