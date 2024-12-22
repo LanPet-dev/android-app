565421e1f7897d8c50145c0b03f274f42ffe9694
sejun2 <qpfjf56@gmail.com>
2024-12-23 04:30:08 +0900

    remove @HiltViewModel to escape KSP error - temp

--- feature/settings/src/main/java/com/lanpet/feature/settings/viewmodel/MemberLeaveViewModel.kt
+++ feature/settings/src/main/java/com/lanpet/feature/settings/viewmodel/MemberLeaveViewModel.kt
@@ -1,9 +1,6 @@
 package com.lanpet.feature.settings.viewmodel
 
 import androidx.lifecycle.ViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
 
-@HiltViewModel
-class MemberLeaveViewModel : ViewModel(){
-    
-}
+// @HiltViewModel
+class MemberLeaveViewModel : ViewModel()
