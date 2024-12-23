0820f27919172bb07e1586f04fcb4c2f8c6393ab
sejun2 <qpfjf56@gmail.com>
2024-12-23 09:30:32 +0900

    add profile update, delete service api

--- data/service/src/main/java/com/lanpet/data/service/ProfileApiService.kt
+++ data/service/src/main/java/com/lanpet/data/service/ProfileApiService.kt
@@ -5,8 +5,12 @@ import com.lanpet.data.dto.FindProfileResponse
 import com.lanpet.data.dto.RegisterManProfileRequest
 import com.lanpet.data.dto.RegisterPetProfileRequest
 import com.lanpet.data.dto.RegisterProfileResponse
+import com.lanpet.data.dto.UpdateProfileRequest
+import com.lanpet.data.dto.UpdateProfileResponse
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.PATCH
 import retrofit2.http.POST
 
 interface ProfileApiService {
@@ -27,4 +31,14 @@ interface ProfileApiService {
 
     @GET("profiles/{id}")
     suspend fun getProfileDetail(id: String): FindProfileDetailResponse
+
+    @PATCH("profiles/{id}")
+    suspend fun updateProfile(
+        id: String,
+        @Body
+        updateProfileRequest: UpdateProfileRequest,
+    ): UpdateProfileResponse
+
+    @DELETE("profiles/{id}")
+    suspend fun deleteProfile(id: String): Unit
 }
