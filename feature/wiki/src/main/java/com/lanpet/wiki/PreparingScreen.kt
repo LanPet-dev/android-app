7ae2903539bb7cebaf2b790e5f621265f4844c92
sejun2 <qpfjf56@gmail.com>
2025-01-02 21:02:23 +0900

    fix textalign

--- feature/wiki/src/main/java/com/lanpet/wiki/PreparingScreen.kt
+++ feature/wiki/src/main/java/com/lanpet/wiki/PreparingScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import com.lanpet.core.designsystem.theme.LanPetAppTheme
@@ -48,6 +49,7 @@ fun PreparingScreen(modifier: Modifier = Modifier) {
                 Text(
                     text = stringResource(R.string.preparing_screen_title),
                     style = MaterialTheme.customTypography().body1SemiBoldMulti,
+                    textAlign = TextAlign.Center,
                 )
             }
         }
