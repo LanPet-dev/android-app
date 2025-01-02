33e2c4fbe35aeb66a030a737d4b72c2b89dc8213
sejun2 <qpfjf56@gmail.com>
2025-01-03 04:29:40 +0900

    refactor add animation

--- feature/wiki/src/main/java/com/lanpet/wiki/PreparingScreen.kt
+++ feature/wiki/src/main/java/com/lanpet/wiki/PreparingScreen.kt
@@ -1,5 +1,12 @@
 package com.lanpet.wiki
 
+import androidx.compose.animation.core.EaseInBounce
+import androidx.compose.animation.core.EaseInOut
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
@@ -14,8 +21,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -26,6 +35,37 @@ import com.lanpet.core.designsystem.theme.customTypography
 
 @Composable
 fun PreparingScreen(modifier: Modifier = Modifier) {
+    val infiniteTransition = rememberInfiniteTransition(label = "infinite")
+    val rotationFloat by infiniteTransition.animateFloat(
+        initialValue = 1f,
+        targetValue = 10f,
+        animationSpec =
+            infiniteRepeatable(
+                animation =
+                    tween(
+                        durationMillis = 900,
+                        easing = EaseInBounce,
+                    ),
+                repeatMode = RepeatMode.Reverse,
+            ),
+        label = "anim",
+    )
+
+    val scaleFloat by infiniteTransition.animateFloat(
+        initialValue = 1f,
+        targetValue = 1.05f,
+        animationSpec =
+            infiniteRepeatable(
+                animation =
+                    tween(
+                        durationMillis = 1500,
+                        easing = EaseInOut,
+                    ),
+                repeatMode = RepeatMode.Reverse,
+            ),
+        label = "anim",
+    )
+
     BoxWithConstraints(
         modifier = Modifier.fillMaxSize(),
     ) {
@@ -44,7 +84,12 @@ fun PreparingScreen(modifier: Modifier = Modifier) {
                     contentDescription = null,
                     modifier =
                         Modifier
-                            .size(200.dp),
+                            .size(200.dp)
+                            .graphicsLayer {
+                                rotationZ = rotationFloat
+                                scaleX = scaleFloat
+                                scaleY = scaleFloat
+                            },
                 )
                 Text(
                     text = stringResource(R.string.preparing_screen_title),
