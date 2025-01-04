29fefc68d19f7ef5ac8db468b8c3ebff6ab1fac3
sejun2 <qpfjf56@gmail.com>
2025-01-04 09:14:44 +0900

    fix some part of layout not showing when keyboard up

--- feature/myprofile/src/main/java/com/lanpet/myprofile/screen/MyProfileManageProfileScreen.kt
+++ feature/myprofile/src/main/java/com/lanpet/myprofile/screen/MyProfileManageProfileScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
@@ -74,6 +75,7 @@ fun MyProfileManageProfileScreen(
     val verticalScrollState = rememberScrollState()
 
     Scaffold(
+        modifier = Modifier.imePadding(),
         topBar = {
             LanPetTopAppBar(
                 navigationIcon = {
