36e57c3f3ec1c8e34d92c483b7e60236adfec5ef
sejun2 <qpfjf56@gmail.com>
2025-01-06 12:25:34 +0900

    fix getFreeBoarPostDetail url path

--- data/service/src/main/java/com/lanpet/data/service/FreeBoardApiService.kt
+++ data/service/src/main/java/com/lanpet/data/service/FreeBoardApiService.kt
@@ -4,13 +4,16 @@ import com.lanpet.domain.model.FreeBoardComment
 import com.lanpet.domain.model.FreeBoardPost
 import com.lanpet.domain.model.FreeBoardPostDetail
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface FreeBoardApiService {
     @GET
     suspend fun getFreeBoardPostList(): List<FreeBoardPost>
 
     @GET
-    suspend fun getFreeBoardPostDetail(id: String): FreeBoardPostDetail
+    suspend fun getFreeBoardPostDetail(
+        @Path("id") id: String,
+    ): FreeBoardPostDetail
 
     @GET
     suspend fun getFreeBoardPostCommentList(id: String): List<FreeBoardComment>
