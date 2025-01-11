0b750180bb9a2e9033896020968de09dea18694f
sejun2 <qpfjf56@gmail.com>
2025-01-11 09:19:53 +0900

    fix TopAppBar's padding  - systemBarsPadding to statusBarsPadding

--- core/common/src/main/java/com/lanpet/core/common/widget/BaseTopAppBar.kt
+++ core/common/src/main/java/com/lanpet/core/common/widget/BaseTopAppBar.kt
@@ -3,6 +3,7 @@ package com.lanpet.core.common.widget
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
@@ -39,7 +40,7 @@ fun LanPetTopAppBar(
         navigationIcon = { navigationIcon?.invoke() },
         actions = actions,
         colors = colors,
-        modifier = modifier.systemBarsPadding(),
+        modifier = modifier.statusBarsPadding(),
     )
 }
 
@@ -87,7 +88,7 @@ fun LanPetCenterAlignedTopAppBar(
         navigationIcon = { navigationIcon?.invoke() },
         actions = actions,
         colors = colors,
-        modifier = modifier.systemBarsPadding(),
+        modifier = modifier.statusBarsPadding(),
     )
 }
 
