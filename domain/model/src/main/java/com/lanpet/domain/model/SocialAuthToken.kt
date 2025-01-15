3d767743ff30e13455aed5b0c49e38a935af6e6e
sejun2 <qpfjf56@gmail.com>
2025-01-15 13:42:58 +0900

    add domain rule for  SocialAuthToken

--- domain/model/src/main/java/com/lanpet/domain/model/SocialAuthToken.kt
+++ domain/model/src/main/java/com/lanpet/domain/model/SocialAuthToken.kt
@@ -7,12 +7,18 @@ data class SocialAuthToken(
     val accessToken: String?,
     val refreshToken: String?,
     val expiresIn: Int?,
-    val expireDateTime: Date = Date(System.currentTimeMillis() + (expiresIn ?: 0) * 1000L),
+    private val expireDateTime: Date = Date(System.currentTimeMillis() + (expiresIn ?: 0) * 1000L),
 ) {
     override fun toString(): String =
-        "SocialAuthToken(socialAuthType=$socialAuthType, accessToken=$accessToken, refreshToken=$refreshToken)"
+        "SocialAuthToken(socialAuthType=$socialAuthType, accessToken=$accessToken, refreshToken=$refreshToken, expiresIn=$expiresIn, expireDateTime=$expireDateTime)"
 
     fun isExpired(): Boolean = expireDateTime.time - System.currentTimeMillis() <= 0
 
     fun shouldRefresh(): Boolean = expireDateTime.time - System.currentTimeMillis() + 1000 * 60 * 3 <= 0
+
+    init {
+        expiresIn?.let {
+            require(expiresIn > 0) { "expiresIn must be greater than 0" }
+        }
+    }
 }
